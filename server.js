f2f1699ced48270f7781f57c2b2c873a02d1c400
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-02-13 14:48:28 +0100

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json()); // aplly midllewares before starting the server
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(cors());
+
+app.listen(port, () => {
+  console.log("Server is running on " + port + "port");
+});
