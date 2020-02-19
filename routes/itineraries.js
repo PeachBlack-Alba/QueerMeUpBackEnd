53c317eefe226fcdc2cd1ef535394d87c1597d26
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-02-19 16:14:47 +0100

    commit

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -5,7 +5,7 @@ router.get("/:city_id", (req, res) => {
   console.log(req.params);
   let itineraryRequested = req.params.city_id;
   itineraryModel
-    .find({ city_id: itineraryRequested })
+    .find({ city_id: itineraryRequested }) // this case find() in cities findeOne() because in here we want all the itineraries of each city
     .then(itinerary => {
       res.send(itinerary);
     })
