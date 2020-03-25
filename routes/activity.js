9084210bd7bb3e80c6f3b5e8289ddbc313b02ee5
loukass23 <l.dupias@net.estia.fr>
2020-03-25 14:31:59 +0100

    update nested favourite object

--- routes/activity.js
+++ routes/activity.js
@@ -5,24 +5,42 @@ const itineraryModel = require("../model/itineraryModel");
 router.post("/addtoFavourite", (req, res) => {
   console.log("req.body", req.body);
   const { itiID, userID, activityname } = req.body;
-  itineraryModel
-    .findById(itiID)
-    .then(itinerary => {
-      //check how to update nested array
-      itinerary.activities.forEach(activity => {
-        if (activity.name === activityname) {
-          if (!activity.favourites.includes(userID)) {
-            activity.favourites.push(userID);
-            console.log("activity", activity);
-            itinerary.save().then(saveditinerary => {
-              res.status(200).send(itinerary);
-            });
-          } else {
-            res.send("You have liked this activity already");
-          }
-        }
-      });
-    })
-    .catch(err => res.send(err));
+
+
+ //in oder to update a nested array you need to use the findOneAndUpdate method from mongoDB as follow
+  let update = { $push: { 'activities.$[activity].favourites': userID } }
+
+  let options = {
+    useFindAndModify: false,
+    upsert: true,
+    arrayFilters: [{ 'activity.name': activityname }]
+  }
+  itineraryModel.findOneAndUpdate(itiID, update, options, (err, itinerary) => {
+   if(err) console.log('err', err)
+   else res.status(200).send(itinerary);
+
+  })
+
+  // itineraryModel
+  //   .findById(itiID)
+  //   .then(itinerary => {
+  //     //check how to update nested array
+
+  //     itinerary.activities.forEach(activity => {
+
+  //       if (activity.name === activityname) {
+  //         if (!activity.favourites.includes(userID)) {
+  //           activity.favourites.push(userID);
+  //           console.log('itinerary', itinerary)
+  //           itinerary.save(err => console.log('err :', err)).then(saveditinerary => {
+  //             res.status(200).send(itinerary);
+  //           });
+  //         } else {
+  //           res.send("You have liked this activity already");
+  //         }
+  //       }
+  //     });
+  //   })
+  //   .catch(err => res.send(err));
 });
 module.exports = router;
